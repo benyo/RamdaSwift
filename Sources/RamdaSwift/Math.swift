6ff6eb45080b1b6b64f0e40a73c4cceb37c26e51
Benjamin Moreno <benyo.mt@gmail.com>
2020-10-11 18:27:44 -0400

    chore: Add Math functions `add, divide, subtract`

--- Sources/RamdaSwift/Math.swift
+++ Sources/RamdaSwift/Math.swift
@@ -7,8 +7,39 @@
 
 import Foundation
 
+let a: Double = 1/2
 
 extension R {
+    public static func add<T: Numeric>(_ a: T, _ b: T) -> T  {
+        return a + b
+    }
+    
+    public static func add<T: Numeric>(_ b: T) -> (T) -> T  {
+        return { a in
+            return a + b
+        }
+    }
+    
+    public static func divide<T: BinaryFloatingPoint>(_ a: T, _ b: T) -> T {
+        return b == 0 ? 0 :  a / b
+    }
+    
+    public static func divide<T: BinaryFloatingPoint>(_ b: T) -> (T) -> T  {
+        return { a in
+            return b == 0 ? 0 :  a / b
+        }
+    }
+    
+    public static func subtract<T: Numeric>(_ a: T, _ b: T) -> T  {
+        return a - b
+    }
+
+    public static func subtract<T: Numeric>(_ b: T) -> (T) -> T  {
+        return { a in
+            return a - b
+        }
+    }
+    
     public static func sum<A: Numeric>(_ a: [A]) -> A {
         return a.reduce(0, {$0 + $1})
     }
--- Tests/RamdaSwiftTests/MathTests.swift
+++ Tests/RamdaSwiftTests/MathTests.swift
@@ -10,6 +10,38 @@ import XCTest
 
 
 final class MathTests: XCTestCase {
+    func testAdd() {
+        
+        let result = R.add(5, 2)
+        let result2 = R.add(2)(5)
+        
+        XCTAssertEqual(result, 7)
+        XCTAssertEqual(result2, 7)
+    }
+    
+    func testDivide() {
+        let num: Double = 5
+        let num2: Double = 2
+        let num3: Double = 0
+        
+        let result = R.divide(num, num2)
+        let result2 = R.divide(num2)(num)
+        let result3 = R.divide(num, num3)
+        
+        XCTAssertEqual(result, 2.5)
+        XCTAssertEqual(result2, 2.5)
+        XCTAssertEqual(result3, 0)
+    }
+    
+    func testSubstract() {
+        
+        let result = R.subtract(5, 2)
+        let result2 = R.subtract(2)(5)
+        
+        XCTAssertEqual(result, 3)
+        XCTAssertEqual(result2, 3)
+    }
+    
     func testSum() {
         let numers = [1,2,3,4]
         
@@ -17,6 +49,7 @@ final class MathTests: XCTestCase {
         
         XCTAssertEqual(result, 10)
     }
+
     static var allTests = [
         ("testSum", testSum),
     ]
